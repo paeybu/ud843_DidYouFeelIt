26b5d9e11df849f04972982d7fb4845a818c4411
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2019-05-23 00:40:10 +0700

    run http on background thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -35,10 +36,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        EarthquakeTask earthquakeTask = new EarthquakeTask();
+        earthquakeTask.execute();
     }
 
     /**
@@ -53,5 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeTask extends AsyncTask<String, Void, Event> {
+
+
+        @Override
+        protected Event doInBackground(String... strings) {
+            return Utils.fetchEarthquakeData(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event result) {
+            updateUi(result);
+        }
     }
 }
